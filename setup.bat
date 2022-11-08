d0730088129fda7b279a4c4f7ea24ff9dedb547e
Phil.Xing <phil.xing@adlinktech.com>
2022-11-08 17:23:51 +0800

    update setup.bat.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,22 @@
 @echo off
-git clone --recurse-submodules -j8 https://github.com/PhilXing/ShellApp.git
+if "%1"=="" (
+    set TARGET_FOLDER=ShellApp
+) else (
+    set TARGET_FOLDER=%1
+)
+
+@REM if exist %TARGET_FOLDER% (
+@REM     set /p answer="Override exist folder?(y/N) "
+@REM     echo %answer%
+@REM     if /I %answer% == y (
+@REM         echo "remove target folder"
+@REM         @REM RMDIR /S /Q %TARGET_FOLDER%
+@REM     ) else (
+@REM         exit 1
+@REM     )
+@REM )
+
+git clone --recurse-submodules -j8 https://github.com/PhilXing/ShellApp.git %TARGET_FOLDER%
 
 @ssh -T git@github.com | findstr /i authenticated 
 if %errorlevel% == 1 (
