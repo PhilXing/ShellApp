922799948f3904017e1e3f5c90d90f89b70b5d04
Phil.Xing <phil.xing@adlinktech.com>
2023-02-08 11:21:12 +0800

    Update batch files.

--- buildapp.bat
+++ buildapp.bat
@@ -2,4 +2,5 @@
 setlocal
 call edk2.bat
 build -a X64 -t VS2019 -b RELEASE -p %~dp0\ShellApp\ShellApp.dsc
+endlocal
 echo on
--- edk2.bat
+++ edk2.bat
@@ -1,4 +1,10 @@
-@set PACKAGES_PATH=%~dp0;%~dp0\edk2
-@set PYTHON_HOME=C:\Python\Python37
-@set PYTHON_COMMAND=C:\Python\Python37\python.exe
-@call edk2\edksetup.bat
+set WORKSPACE=%cd%
+set NASM_PREFIX=C:\NASM\
+set PACKAGES_PATH=%~dp0;%~dp0\edk2
+set PYTHON_HOME=C:\Python\Python38
+set PYTHON_COMMAND=C:\Python\Python38\python.exe
+set HOST_ARCH=X64
+call edk2\edksetup.bat
+if not exist %WORKSPACE%\edk2\BaseTools\Source\C\bin (
+    make -C edk2\BaseTools
+)
